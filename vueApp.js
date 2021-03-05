e7ed423d2b014a7ab89c60b91876bd7b901d48eb
x7md <it7mdx@googlemail.com>
2021-03-05 14:27:48 +0300

    add data and set vueJS

--- vueApp.js
+++ vueApp.js
@@ -1,7 +1,65 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hello Vue!'
+let main =  Vue.component('cm-main', {
+    data: function () {
+      return {
+        arr: []
+      }
+    },
+    mounted: function () {
+      fetch("data/feed-home.json").then((rep)=> rep.json()).then((json_) => this.arr = json_)
     }
+    ,
+    template: `
+    <div class="row">
+    <div class="col">
+              <!--
+                Cards
+              -->
+              <div v-for="item in arr" :key="item.title">
+              <div class="card" :disabled="!item.link">
+                <div class="card-body d-inline-flex">
+                  <div class="info-body">
+                    <h5 class="card-title">{{ item.title }}</h5>
+                  <p class="card-text">{{ item.description }}</p>
+                  </div>
+                  <button class="btn me-auto go-to">
+                    <span class="material-icons-round">west</span>
+                  </button>
+                </div>
+              </div>
+              </div>
+              <!--
+                Cards
+              -->
+              <!--
+                Info
+              -->
+            </div>
+            <div class="col-4 m-auto">
+              <div class="card info">
+                <div class="card-body">
+                  <h5 class="card-title">ضايع وحايس في الكيمياء؟</h5>
+                  <p class="card-text">شُّويّة كيمياء هنا عشان تساعدك... عشان تستذكر ما نسيته أو ما لم تفهمه بشكل جيد...</p>
+                  <a href="#" class="card-link">عن المشروع</a>
+                </div>
+              </div>
+            </div>
+            <!--
+                Info
+              -->
+            </div>
+            </div>
+    `
+  })
+
+  const routes = [
+    { path: '/', component: main },
+  ]
+  
+  const router = new VueRouter({
+    routes
   })
   
+  
+  const app = new Vue({
+    router
+  }).$mount('#app')
